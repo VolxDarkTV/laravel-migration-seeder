0f1d206d4377f4142e8031320d5ec5dc290364df
VolxDarkTV <vinvenzo.foti@outlook.it>
2023-02-01 15:16:34 +0100

    add travel table

--- laravel-migration-seeder/database/migrations/2023_02_01_114535_create_train_table.php
+++ laravel-migration-seeder/database/migrations/2023_02_01_114535_create_train_table.php
@@ -16,12 +16,18 @@ return new class extends Migration
         Schema::create('train', function (Blueprint $table) {
             $table -> id();
             $table -> string('run_name') -> nullable(true);
-            $table -> integer('run_number') -> unsigned();
+            $table -> integer('run_number') -> unique() -> unsigned();
             $table -> integer('price') -> unsigned();
             $table -> string('location_start');
             $table -> string('location_destination');
             $table -> date('start_time');
             $table -> date('end_time');
+            $table -> timestamps();
+        });
+        Schema::create('travel', function (Blueprint $table) {
+            $table -> id();
+            $table -> foreign('train_id') -> references('id') -> on('train');
+            $table -> string('agency_name');
             $table -> timestamps();
         });
     }
